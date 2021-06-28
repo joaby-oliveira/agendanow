3de769f8bd1a423fd1b1230df491c74afcf8ecf8
Joaby <joabyoliveira2004@gmail.com>
2021-06-28 20:29:20 -0300

    feat: set up database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 app.use(express.static('public'))
 
@@ -8,6 +9,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.set('view engine', 'ejs')
+
+mongoose.connect("mongodb://localhost:27017/scheduling", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 app.get('/', (req, res) => {
   res.send('e ae meu rei')
